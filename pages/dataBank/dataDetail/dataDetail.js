b40ccbd426a1ebab427f9869ab970f7547be45ac
lkjtzsq <879932397@qq.com>
2020-06-04 00:48:49 +0800

    修改兑换素材类型判断

--- pages/dataBank/dataDetail/dataDetail.js
+++ pages/dataBank/dataDetail/dataDetail.js
@@ -8,15 +8,15 @@ Page({
 
   },
   //关闭视频
-  closeVideo(){
+  closeVideo() {
     this.setData({
-      videoSrc:""
+      videoSrc: ""
     })
   },
   //积分兑换
-  convert(){
+  convert() {
     var token = wx.getStorageSync('token')
-    var that=this
+    var that = this
     wx.request({
       url: app.globalData.studentBase + '/api/pay/jifen/material',
       method: "POST",
@@ -26,35 +26,36 @@ Page({
       data: {
         material_id: that.data.id
       },
-      success:function(res){
+      success: function(res) {
         console.log(res.data.meterial_source)
         wx.showToast({
           title: '素材下载中...',
-          icon:'loading',
-          duration:160000
+          icon: 'loading',
+          duration: 160000
         })
+        var material_type = res.data.material_type
         wx.downloadFile({
           // 示例 url，并非真实存在
           url: res.data.meterial_source,
-          success: function (res) {
+          success: function(res) {
             console.log('下载成功')
             const filePath = res.tempFilePath
             console.log(filePath)
-            if((filePath.indexOf(".jpg") !=-1) || (filePath.indexOf(".png") !=-1) || (filePath.indexOf(".gif") !=-1) || (filePath.indexOf(".jpg") !=-1)){
+            if (material_type == "image") {
               wx.previewImage({
                 urls: [filePath]
               })
-            }else if(filePath.indexOf(".mp4") !=-1){
+            } else if (material_type == "media") {
               that.setData({
-                videoSrc:filePath
+                videoSrc: filePath
               })
-            }else{
+            } else {
               wx.openDocument({
                 filePath: filePath,
-                success: function (res) {
+                success: function(res) {
                   console.log('打开文档成功')
                 },
-                fail:function(err){
+                fail: function(err) {
                   console.log(err)
                   console.log('打开文档失败')
                 }
@@ -62,22 +63,22 @@ Page({
             }
             wx.hideToast()
           },
-          fail:function(err){
+          fail: function(err) {
             console.log('下载失败')
             console.log(err)
           }
         })
-        
+
       }
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var id = options.id
     this.setData({
-      id:id
+      id: id
     })
     var token = wx.getStorageSync('token')
     var that = this
@@ -93,7 +94,7 @@ Page({
       success: function(res) {
         console.log(res)
         that.setData({
-          detail:res.data.data
+          detail: res.data.data
         })
       }
     })
@@ -102,49 +103,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
